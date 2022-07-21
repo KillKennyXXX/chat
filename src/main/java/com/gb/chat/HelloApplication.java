074a23401c80b14cde70ccde191b666fc4361cab
Alexey Shalygin <84471084+KillKennyXXX@users.noreply.github.com>
2022-07-21 18:27:57 +0300

    1

--- src/main/java/com/gb/chat/HelloApplication.java
+++ src/main/java/com/gb/chat/HelloApplication.java
@@ -11,10 +11,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chat.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 746, 542);
+        stage.setTitle("Chat++");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setX(200);
+        stage.setY(200);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/gb/chat/HelloController.java
+++ src/main/java/com/gb/chat/HelloController.java
@@ -1,14 +1,72 @@
 package com.gb.chat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<String> chatList;
+
+    @FXML
+    private VBox chatWindow;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private ImageView logoImg;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private TextField msgText;
+
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private Label userID;
+
+    @FXML
+    private ListView<String> userList;
+
+    @FXML
+    void onClear(ActionEvent event) {
+        msgText.setText("");
     }
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSend(ActionEvent event) {
+        if (msgText.getText().trim() != "") {
+            chatList.getItems().add(msgText.getText().trim());
+            msgText.setText("");
+        }
+
+    }
+    @FXML
+    void onMsgKeyPress(KeyEvent event) {
+
+        if (msgText.getText().trim() != "" && event.getCode() == KeyCode.ENTER) {
+            chatList.getItems().add(msgText.getText().trim());
+            msgText.setText("");
+        }
+    }
+
+
 }
